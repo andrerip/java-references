3c658e35c40491d881d2b71213a71d03176ce182
André Ripari <aripari@veloe.com.br>
2020-04-23 12:30:01 -0300

    nova classe de infinite range do site do Hyperskill

---
+++ src/main/java/com/andrerip/javaref/java8/InfiniteRange.java
@@ -0,0 +1,25 @@
+package com.andrerip.javaref.java8;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+class InfiniteRange {
+	
+	static AtomicInteger atomicInteger;
+	
+    public static Supplier<Integer> getInfiniteRange() {
+    	if(atomicInteger==null) {
+    		atomicInteger = new AtomicInteger();
+    	}
+    	return () -> atomicInteger.getAndIncrement();
+    }
+    
+    public static void main(String[] args) {
+    	Supplier<Integer> sup1 = getInfiniteRange();
+    	Supplier<Integer> sup2 = getInfiniteRange();
+    	 
+    	for(int i = 0; i < 5; i++) {
+    	    System.out.print(sup1.get() + " " + sup2.get() + " ");
+    	}
+	}
+}
