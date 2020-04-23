b2b95e568a8349ad112f4a1eb2654241352a3023
André Ripari <andrerip@gmail.com>
2020-04-23 12:20:53 -0300

    criação estrutura inicial do projeto

---
+++ src/main/java/com/andrerip/javaref/JavaReferencesApplication.java
@@ -0,0 +1,13 @@
+package com.andrerip.javaref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReferencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReferencesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrerip/javaref/JavaReferencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrerip.javaref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReferencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
